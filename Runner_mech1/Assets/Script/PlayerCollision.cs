995cebb223405ca2f47fd1eae26c5202d16ff1ad
elagame <72454253+elagame@users.noreply.github.com>
2021-08-16 12:13:41 +0300

    mechanic fixed
    buttons and level5 added

---
+++ Runner_mech1/Assets/Script/PlayerCollision.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+
+    public PlayerMovement movement;
+
+    void OnCollisionEnter(Collision Col)
+    {
+        if (Col.collider.tag == "Obstacle")
+        {
+            movement.enabled = false;
+
+            Debug.Log("hit");
+        }
+
+
+    }
+}
---
+++ Runner_mech1/Assets/Script/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public Rigidbody rd;
+    public float Speed;
+    public float Swingspeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        rd.AddForce(0,0, Speed* Time.deltaTime);
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey("a"))
+        {
+            rd.AddForce(-Swingspeed * Time.deltaTime, 0, 0, ForceMode.VelocityChange );
+
+        }
+        if (Input.GetKey("d"))
+        {
+            rd.AddForce(Swingspeed * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+
+        }
+    }
+}
